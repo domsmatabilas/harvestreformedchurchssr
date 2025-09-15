56e10a7c1e08cf61b04190993e93f22d7aea695c
Dominic Matabilas <dominicmatabilas@gmail.com>
2025-09-15 16:39:15 +1000

    update metas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,62 +17,32 @@ export const routes: Routes = [
     {
         path: '', 
         component: HomePage, 
-        pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church',
-            description: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.',
-            keywords: 'Harvest Reformed Church, Harvest, Reformed, Church, Werribee, Victoria, Australia, Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        pathMatch: 'full'
     },
     {
         path: 'sermons', 
         component: SermonsPage, 
-        pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church - Sermons',
-            description: 'Every Sunday, we record our sermons so other people will also hear the truth from God`s word. You can find our past sermons on this page.',
-            keywords: 'Harvest Reformed Church Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        pathMatch: 'full'
     },
     {
         path: 'contact-us', 
         component: ContactUsPage, 
-        pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church',
-            description: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.',
-            keywords: 'Harvest Reformed Church, Harvest, Reformed, Church, Werribee, Victoria, Australia, Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        pathMatch: 'full'
     },
     {
         path: 'sunday-service', 
         component: SundayServicePage, 
-        pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church',
-            description: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.',
-            keywords: 'Harvest Reformed Church, Harvest, Reformed, Church, Werribee, Victoria, Australia, Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        pathMatch: 'full'
     },
     {
         path: 'bible-study', 
         component: BibleStudyPage, 
-        pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church',
-            description: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.',
-            keywords: 'Harvest Reformed Church, Harvest, Reformed, Church, Werribee, Victoria, Australia, Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        pathMatch: 'full'
     },
     {
         path: 'who-we-are', 
         component: WhoWeArePage, 
-        pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church',
-            description: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.',
-            keywords: 'Harvest Reformed Church, Harvest, Reformed, Church, Werribee, Victoria, Australia, Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        pathMatch: 'full'
     },
     {
         path: 'what-we-believe', 
@@ -80,31 +50,16 @@ export const routes: Routes = [
     },
     {
         path: 'leadership', component: 
-        LeadershipPage, pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church',
-            description: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.',
-            keywords: 'Harvest Reformed Church, Harvest, Reformed, Church, Werribee, Victoria, Australia, Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        LeadershipPage, pathMatch: 'full'
     },
     {
         path: 'contact-us', 
         component: ContactUsPage, 
-        pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church',
-            description: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.',
-            keywords: 'Harvest Reformed Church, Harvest, Reformed, Church, Werribee, Victoria, Australia, Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        pathMatch: 'full'
     },
     {
         path: 'give', 
         component: GivePage, 
-        pathMatch: 'full',
-        data: {
-            title: 'Harvest Reformed Church',
-            description: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.',
-            keywords: 'Harvest Reformed Church, Harvest, Reformed, Church, Werribee, Victoria, Australia, Sermons, Bible Study, Sunday Service, Contact Us, Give'
-        }
+        pathMatch: 'full'
     }
 ];
--- src/app/sunday-service-page/sunday-service-page.ts
+++ src/app/sunday-service-page/sunday-service-page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card-component/card-component';
+import { Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-sunday-service-page',
@@ -9,4 +11,16 @@ import { CardComponent } from '../card-component/card-component';
 })
 export class SundayServicePage {
 
+  constructor(
+      private meta: Meta
+    ) {
+      this.meta.removeTag('name="description"');
+      this.meta.removeTag('name="keywords"');
+      this.meta.removeTag('name="title"');
+      this.meta.addTags([
+        { name: 'title', content: 'Harvest Reformed Church - Sunday Service' },
+        { name: 'description', content: 'This is the official website of Harvest Reformed Church. We believe and strongly uphold the the five solas of the Reformation, which distinguished the Reformers from the teachings of Rome, include sola scriptura (Scripture alone), solus Christus (Christ alone), sola fide (faith alone), sola gratia (grace alone), and soli Deo gloria (glory to God alone). Harvest Reformed Church is located in Werribee, Victoria 3030 Australia.' },
+        { name: 'keywords', content: 'Harvest Reformed Church Sunday Service, Sunday Service, Sunday Worship' }
+      ]);
+    }
 }
